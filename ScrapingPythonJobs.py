91ac9482138cd61c4b89509e0faf200837ee655d
Igor <45922551+IgorGreb@users.noreply.github.com>
2021-02-25 21:25:43 +0200

    Update ScrapingPythonJobs.py

--- ScrapingPythonJobs.py
+++ ScrapingPythonJobs.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import time
 
 
 
@@ -10,20 +11,29 @@ def find_python_vacancy():
 	soup = BeautifulSoup(req, 'lxml')
 	jobs = soup.find_all('div', class_="card card-hover card-visited wordwrap job-link")
 
-	for job in jobs:
+	for index, job in enumerate(jobs):
 		company_name = job.find('div', class_='add-top-xs').text
 		Language = job.find('h2').text
 		text = job.find('p', class_='overflow text-muted add-top-sm add-bottom').text
 		date_ = job.find('span', class_= 'text-muted small').text
 		for el in job.select('h2'):
 			url_more = el.select_one('a').get('href')
-		print('-----------------------------------------------------')
-		print(f'''
-COMPANY NAME: {company_name.strip()}
-LANGUAGE: {Language.strip()}
-INFORMATION: {text.strip()}
-DATE: {date_.strip()}
-URL: https://www.work.ua{url_more}''')
+
+		with open(f'src/{index}text', 'w') as f:
+			f.write(f'COMPANY NAME: {company_name.strip()}\n')
+			f.write(f'LANGUAGE: {Language.strip()}\n')
+			f.write(f'INFORMATION: {text.strip()}\n')
+			f.write(f'DATE: {date_.strip()}\n')
+			f.write(f'URL: https://www.work.ua{url_more}\n')
+		print('File Saved')
 
 
 find_python_vacancy()
+
+if __name__ == '__main__':
+	while True:
+		find_python_vacancy()
+		time_wait = 10
+		print(f'Waitting {time_wait} minutes...')
+		time.sleep(time_wait * 60)
+	
